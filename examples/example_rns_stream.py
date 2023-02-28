eb7ece4e458466fb7bae21439cbd3b035f89917f
timonmerk <merkimi1995@googlemail.com>
2023-02-28 03:39:11 -0500

    fix joblib

--- examples/example_rns_stream.py
+++ examples/example_rns_stream.py
@@ -16,13 +16,13 @@ import seaborn as sb
 import mne
 from scipy import stats
 import pandas as pd
+from joblib import Memory
+from joblib import Parallel, delayed
 
 # Nexus2/RNS_DataBank/PITT/PIT-RNS1534/iEEG
 # PIT-RNS1534_PE20161220-1_EOF_SZ-NZ.EDF
 
-
-def init_pynm_rns_stream():
-    # basic init settings that will initialize the stream
+def init_stream_all():
     channels = ["ch1", "ch2", "ch3", "ch4"]
     sfreq = 250
 
@@ -53,6 +53,7 @@ def init_pynm_rns_stream():
         "low beta": [13, 20],
         "high beta": [20, 35],
         "low gamma": [35, 60],
+        "broadband" : [20, 120]
     }
 
     # INIT Feature Estimation Time Window Length and Frequency
@@ -65,14 +66,35 @@ def init_pynm_rns_stream():
 
     # ENABLE feature types
     stream.settings["features"]["fft"] = True
-    stream.settings["features"]["fooof"] = False
-    stream.settings["features"]["linelength"] = False
-    stream.settings["features"]["sharpwave_analysis"] = False
+    stream.settings["features"]["linelength"] = True
+    stream.settings["features"]["sharpwave_analysis"] = True
+    stream.settings["features"]["bursts"] = True
     stream.settings["features"]["mne_connectiviy"] = False
+    stream.settings["features"]["fooof"] = True
+    stream.settings["fooof"]["periodic"]["center_frequency"] = False
+    stream.settings["fooof"]["periodic"]["height_over_ap"] = False
+    stream.settings["fooof"]["periodic"]["band_width"] = False
+    stream.settings["fooof"]["windowlength_ms"] = 500000
 
-    # SPECIFY specific feature modalities
-    stream.settings["mne_connectiviy"]["method"] = "plv"
-    stream.settings["mne_connectiviy"]["mode"] = "multitaper"
+    stream.settings["features"]["coherence"] = True
+    stream.settings["coherence"]["channels"] = [
+        [
+            "ch1",
+            "ch2"
+        ],  
+        [
+            "ch1",
+            "ch3"
+        ],
+        [
+            "ch3",
+            "ch4"
+        ],
+    ]
+    stream.settings["coherence"]["frequency_bands"] = [
+        "high beta",
+        "low gamma"
+    ]
 
     stream.settings["fft_settings"]["windowlength_ms"] = 500000
     stream.settings["fft_settings"]["log_transform"] = True
@@ -113,33 +135,49 @@ def init_pynm_rns_stream():
         "prominence",
     ]
     stream.settings["sharpwave_analysis_settings"]["filter_ranges_hz"] = [
-        [5, 30],
-        [5, 60],
+        [5, 30],[5, 60]
     ]
-
-    stream.settings["fooof"]["periodic"]["center_frequency"] = False
-    stream.settings["fooof"]["periodic"]["band_width"] = False
-    stream.settings["fooof"]["periodic"]["height_over_ap"] = False
-    stream.settings["fooof"]["windowlength_ms"] = 500000
 
     stream.init_stream(
         sfreq=sfreq,
         line_noise=60,
     )
-
     return stream
 
+def run_sub(sub_cohort):
+    sub = sub_cohort[0]
+    PATH_COHORT = sub_cohort[1]
+    PATH_SUB = os.path.join(PATH_COHORT, sub)
+    files = [os.path.join(PATH_SUB, f) for f in os.listdir(PATH_SUB) if '.dat' in f]
+    # stream = init_pynm_rns_stream()
+    stream = init_stream_all()
+    PATH_OUT = os.path.join(PATH_OUT_BASE, cohort)
+    try:
+        stream.run(files, PATH_OUT, folder_name=sub)
+    except:
+        print(f'could not run {cohort} {sub}')
 
 if __name__ == "__main__":
 
-    PATH_BASE = r"C:\Users\ICN_admin\Documents\Datasets\RNS_Test\MGH"
-    PATH_OUT = r"C:\Users\ICN_admin\Documents\Datasets\RNS_Test\OUT_Features"
-    sub_name = "RNS2019"
-    PATH_READ = os.path.join(PATH_BASE, sub_name)
+    PATH_BASE = 'Z:\\RNS_data\\ecog_reconstructed\\'
+    PATH_OUT_BASE = r'X:\Users\timon\Ashley_pynm_datfiles\OUT_example_without_fooof'
+    l_sub_cohort = []
+    for cohort in ['MTS', 'MSR', 'PIT', 'MGH']:
+        PATH_COHORT = os.path.join(PATH_BASE, cohort)
+        subjects = [s for s in os.listdir(PATH_COHORT) if 'RNS' in s and os.path.exists(os.path.join(PATH_OUT_BASE, cohort, s)) is False]
+        for sub in subjects:
+            l_sub_cohort.append((sub, PATH_COHORT))
+    #run_sub(l_sub_cohort[0])
 
-    list_dat_file_paths = [
-        os.path.join(PATH_READ, f) for f in os.listdir(PATH_READ)
-    ]
-
-    stream = init_pynm_rns_stream()
-    stream.run(list_dat_file_paths, PATH_OUT, folder_name=sub_name)
+    # multiprocessing
+    #location = './cachedir'
+    #memory = Memory(location, verbose=0)
+    #costly_compute_cached = memory.cache(run_sub)
+    #def data_processing_mean_using_cache(sub_cohort):
+    #    """Compute the mean of a column."""
+    #    return run_sub(sub_cohort)
+    Parallel(n_jobs=17)(
+        delayed(run_sub)(sub_cohort)
+        for sub_cohort in l_sub_cohort
+    )
+    #memory.clear(warn=False)
